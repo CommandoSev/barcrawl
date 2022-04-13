ec2d99f97c1cc355ba8c83f349af0673cfae3022
Camran <c4mr4nduffy@googlemail.com>
2022-04-13 23:36:44 +0200

    Added users, attendee_mapping and events tables

--- database/postgres/sql_scrips.sql
+++ database/postgres/sql_scrips.sql
@@ -1,1 +1,22 @@
-CREATE TABLE events (id INTEGER PRIMARY KEY, name VARCHAR);
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY, 
+    username VARCHAR, 
+    icon BYTEA,
+    location FLOAT[]
+    );
+
+CREATE TABLE attendee_mapping (
+    attendee_id INTEGER, 
+    event_id INTEGER,
+    PRIMARY KEY (attendee_id, event_id)
+    );
+
+CREATE TABLE events (
+    event_id INTEGER PRIMARY KEY, 
+    owner_id INTEGER, 
+    start_location FLOAT[],
+    waypoints FLOAT[][],
+    end_location FLOAT[],
+    creation_date TIMESTAMPTZ,
+    start_date TIMESTAMPTZ
+    );
