a35e788385f68aead923f5cb715a97c497ea2fab
Camran <c4mr4nduffy@googlemail.com>
2022-04-14 15:33:35 +0200

    Added manager interface to handle requests and interact with DB

--- database/manager/manager.py
+++ database/manager/manager.py
@@ -1,13 +1,12 @@
 
 from flask import Flask, request
 import sqlalchemy as sql
-from sqlalchemy import text
+from sqlalchemy import text, insert
 from datetime import datetime, timedelta
-import json
 
 DATABASE_URL = "postgresql://postgres:password@postgresservice/postgres"
-
 engine = sql.create_engine(DATABASE_URL)
+
 
 app = Flask(__name__)
 
@@ -22,36 +21,37 @@ def hello():
         }
     })
 
+@app.route("/add_test", methods=['GET'])
+def add_test():
+    sql_query = "CREATE TABLE test (test_id INTEGER, test2_id INTEGER)"
 
-# managerservice:5433/add_event?event_id=1&owner_id=1
-
-@app.route("/add_user", methods=['POST'])
-def add_user():
-    username = request.args['username']
-    icon = request.args['icon']
-    location = request.args['location']
-    return True
+    with engine.connect() as conn:
+        conn.execute(text(sql_query))
+        conn.close()
+    
+    return 'Sucess: Made table'
 
 
-@app.route("/add_event", methods=['POST'])
+@app.route("/add_event", methods=['GET'])
 def add_event():
-    event_id = request.args['event_id']
-    owner_id = request.args['owner_id']
+    event_id = 1
+    owner_id = 1
     start_location = [0, 0]
     waypoints = [[0, 20],[20, 20],[40, 20]]
     end_location = [40, 40]
     creation_date = datetime.now()
-    start_date = datetime.now() + timedelta(days=10)
 
-    sql_query = "INSERT INTO events ({} INTEGER, {} INTEGER, {} FLOAT[], {} FLOAT[][], {} FLOAT[], {} TIMESTAMPTZ, {} TIMESTAMPTZ)".format(event_id, owner_id, start_location, waypoints, end_location, creation_date, start_date)
+    sql_query = "INSERT INTO events (event_id, owner_id, start_location, waypoints, end_location, creation_date, start_date) VALUES ({}, {}, :strt, :way, :endl, CURRENT_TIMESTAMP, (CURRENT_TIMESTAMP - interval '10 day'));".format(event_id, owner_id, creation_date)
 
     with engine.connect() as conn:
-        conn.execute(text(sql_query))
-        conn.commit()
-        conn.close()
-    
-    return True
+            conn.execute(text(sql_query), strt=start_location, way=waypoints, endl=end_location)
+            conn.close()
+        
+    return 'Made Event'
+
+
+# # managerservice:5433/add_event?event_id=1&owner_id=1
 
 
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5433, debug=True)
