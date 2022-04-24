22046351e6ce3d7b4081b54e5f40d75209b902c5
Camran <c4mr4nduffy@googlemail.com>
2022-04-24 12:38:54 +0100

    Added new endpoints to interact with the DB with

--- database/manager/manager.py
+++ database/manager/manager.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import sqlalchemy as sql
 from sqlalchemy import text, insert
 from datetime import datetime, timedelta
+import json
 
 DATABASE_URL = "postgresql://postgres:password@postgresservice/postgres"
 engine = sql.create_engine(DATABASE_URL)
@@ -11,26 +12,91 @@ engine = sql.create_engine(DATABASE_URL)
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def hello():
-    return nice_json({
-        "uri": "/",
-        "subresource_uris": {
-            "movies": "/movies",
-            "movie": "/movies/<id>"
-        }
-    })
+@app.route("/get_events", methods=['GET'])
+def get_events():
 
-@app.route("/add_test", methods=['GET'])
-def add_test():
-    sql_query = "CREATE TABLE test (test_id INTEGER, test2_id INTEGER)"
+    sql_query = "SELECT * FROM events"
 
     with engine.connect() as conn:
-        conn.execute(text(sql_query))
+        resultproxy = conn.execute(text(sql_query))
         conn.close()
-    
-    return 'Sucess: Made table'
 
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    json_format = json.dumps(a, default=str)
+
+    return json_format
+
+@app.route("/get_attendee_mapping", methods=['GET'])
+def get_attendee_mapping():
+
+    sql_query = "SELECT * FROM attendee_mapping"
+
+    with engine.connect() as conn:
+        resultproxy = conn.execute(text(sql_query))
+        conn.close()
+
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    json_format = json.dumps(a, default=str)
+
+    return json_format
+
+@app.route("/get_users", methods=['GET'])
+def get_users():
+
+    sql_query = "SELECT * FROM users"
+
+    with engine.connect() as conn:
+        resultproxy = conn.execute(text(sql_query))
+        conn.close()
+
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    json_format = json.dumps(a, default=str)
+
+    return json_format
+
+@app.route("/get_user", methods=['GET'])
+def get_user():
+
+    user_id = request.args.get('user_id')
+
+    sql_query = "SELECT * FROM users WHERE user_id = {}".format(user_id)
+
+    with engine.connect() as conn:
+        resultproxy = conn.execute(text(sql_query))
+        conn.close()
+
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    json_format = json.dumps(a, default=str)
+
+    return json_format
 
 @app.route("/add_event", methods=['GET'])
 def add_event():
@@ -48,6 +114,29 @@ def add_event():
             conn.close()
         
     return 'Made Event'
+
+@app.route("/get_attendee_locations", methods=['GET'])
+def get_attendee_locations():
+
+    event_id = request.args.get('event_id')
+
+    sql_query = "SELECT * FROM users WHERE user_id IN (SELECT attendee_id FROM attendee_mapping WHERE event_id = {})".format(event_id)
+
+    with engine.connect() as conn:
+        resultproxy = conn.execute(text(sql_query))
+        conn.close()
+
+    d, a = {}, []
+    for rowproxy in resultproxy:
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        for column, value in rowproxy.items():
+            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    json_format = json.dumps(a, default=str)
+
+    return json_format
 
 
 # # managerservice:5433/add_event?event_id=1&owner_id=1
--- database/postgres/sql_scrips.sql
+++ database/postgres/sql_scrips.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY, 
     username VARCHAR, 
-    icon BYTEA,
+    icon VARCHAR,
     location FLOAT[]
     );
 
@@ -20,3 +20,20 @@ CREATE TABLE events (
     creation_date TIMESTAMPTZ,
     start_date TIMESTAMPTZ
     );
+
+INSERT INTO events (event_id, owner_id, start_location, waypoints, end_location, creation_date, start_date) VALUES (0001, 0001, '{1.0, 1.0}', '{{2.0, 2.0}, {3.0, 3.0}}', '{4.0, 4.0}', CURRENT_TIMESTAMP, (CURRENT_TIMESTAMP - interval '10 day'));
+INSERT INTO events (event_id, owner_id, start_location, waypoints, end_location, creation_date, start_date) VALUES (0002, 0001, '{20.0, 20.0}', '{{21.0, 21.0}, {22.0, 22.0}}', '{23.0, 23.0}', CURRENT_TIMESTAMP, (CURRENT_TIMESTAMP - interval '10 day'));
+
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0002, 0001);
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0003, 0001);
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0004, 0001);
+
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0003, 0002);
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0004, 0002);
+INSERT INTO attendee_mapping (attendee_id, event_id) VALUES (0005, 0002);
+
+INSERT INTO users (user_id, username, icon, location) VALUES (0001, 'alice', 'icon1', '{1.0, 2.0}');
+INSERT INTO users (user_id, username, icon, location) VALUES (0002, 'bob', 'icon1', '{2.0, 3.0}');
+INSERT INTO users (user_id, username, icon, location) VALUES (0003, 'charlie', 'icon1', '{3.0, 4.0}');
+INSERT INTO users (user_id, username, icon, location) VALUES (0004, 'daron', 'icon1', '{4.0, 5.0}');
+INSERT INTO users (user_id, username, icon, location) VALUES (0005, 'ella', 'icon1', '{5.0, 6.0}');
